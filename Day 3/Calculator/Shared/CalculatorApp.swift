2bd0a03e7c196142de56b0580752d9a32118fcd1
Marcus Meekins <meeks_mha@yahoo.com>
2022-08-24 16:00:54 -0400

    Calculator

---
+++ Day 3/Calculator/Shared/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Shared
+//
+//  Created by Consultant on 8/19/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
